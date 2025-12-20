3452a1880c510075bde97f39ee1da7073219f95e
caevzz <zenoreeamyc@gmail.com>
2025-12-20 21:46:18 +0800

    Add

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "ZyrooNour",
+  description: "Personal website",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  );
+}
